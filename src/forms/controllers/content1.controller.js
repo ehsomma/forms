1f5ccac3d974a69856dfaaba2602f9481ceca740
ehsomma <ehsomma@gmail.com>
2023-05-18 23:05:27 -0300

    scroll

--- src/forms/controllers/content1.controller.js
+++ src/forms/controllers/content1.controller.js
@@ -1,18 +1,27 @@
-import { createElementWithTemplate } from './componentLoader.js';
+import { createElementWithTemplate } from "./componentLoader.js";
 
-const Template = 'content1'; // Html template.
+const Template = "content1"; // Html template.
 
 export default async () => {
     // Creates new div element with the specified template.
     const divElement = await createElementWithTemplate(Template);
-    
+
     // Adds listeners.
-    const boton1 = divElement.querySelector('#boton1');
-    boton1.addEventListener('click', (event) => {
+    const boton1 = divElement.querySelector("#boton1");
+    boton1.addEventListener("click", (event) => {
         event.preventDefault();
-        alert('hola 1');
+        alert("hola 1");
     });
+
+    // Textarea autoincrement.
+    const textarea = divElement.querySelector("#textArea1");
+    var limit = 175; // Height limit.
+
+    textarea.oninput = () => {
+        textarea.style.height = "";
+        textarea.style.height = Math.min(textarea.scrollHeight, limit) + "px";
+        console.log('textarea');
+    };
 
     return divElement;
 };
-
